a65fbe5112b6ff86eaab97c54f25c0d516c1ac50
Shahin4292 <sarker4292@gmail.com>
2025-04-13 14:29:54 +0600

    add user

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -10,7 +10,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: false,
         automaticallyImplyLeading: false,
@@ -22,18 +22,74 @@ class HomeView extends StatelessWidget {
             Icon(Icons.menu),
             Text(
               "Dashboard",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
           ],
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-          ],
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: EdgeInsets.only(left: 14, right: 14),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 10),
+                width: MediaQuery.sizeOf(context).width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE6EAF3),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(6),
+                          topLeft: Radius.circular(6),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.people_alt,
+                            color: Colors.deepPurple,
+                            size: 20,
+                          ),
+                          SizedBox(width: 8),
+                          Text('Users', style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      margin: EdgeInsets.only(left: 14, right: 14, top: 14),
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF1F4FB),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: Column(
+                        children: [
+                          Text('26', style: TextStyle(fontSize: 34)),
+                          Text(
+                            'Users',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
